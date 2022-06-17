f1097fe3875c719abda69128552ffed0202877cc
JeUnsong <zzangjh5@gmail.com>
2022-06-17 18:12:12 +0900

    프로젝트 최초 파일
    아직 설정 안됨

---
+++ src/main/java/com/mangjakseon/MangjakseonApplication.java
@@ -0,0 +1,13 @@
+package com.mangjakseon;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MangjakseonApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MangjakseonApplication.class, args);
+    }
+
+}
