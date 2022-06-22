d21e7861f03102b53941539d79831f9f47220d5b
JeUnsong <zzangjh5@gmail.com>
2022-06-22 15:25:28 +0900

    review 페이지 작업

--- src/main/java/com/mangjakseon/controller/ReviewController.java
+++ src/main/java/com/mangjakseon/controller/ReviewController.java
@@ -2,7 +2,11 @@ package com.mangjakseon.controller;
 
 import lombok.extern.log4j.Log4j2;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 @Log4j2
@@ -12,4 +16,18 @@ public class ReviewController {
     public void review(){
 
     }
+
+    @RequestMapping("/review/{contentId}")
+    public String Login(@PathVariable String contentId, Model model){
+
+        model.addAttribute("contentId", contentId);
+
+        return "/review";
+    }
+
+
+//    @PostMapping("/review/${contentId}")
+//    public String reviewId(){
+//        return "";
+//    }
 }
