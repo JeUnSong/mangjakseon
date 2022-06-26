c517d34f8343d3264437962d42cb15e7b199e542
JeUnSong <94476600+JeUnSong@users.noreply.github.com>
2022-06-26 19:40:11 +0900

    json 데이터 어떻게 전달할지 고민중

--- src/main/java/com/mangjakseon/controller/ReviewController.java
+++ src/main/java/com/mangjakseon/controller/ReviewController.java
@@ -2,6 +2,7 @@ package com.mangjakseon.controller;
 
 
 import com.mangjakseon.dto.MovieDTO;
+import com.nimbusds.jose.shaded.json.JSONArray;
 import com.nimbusds.jose.shaded.json.JSONObject;
 import com.nimbusds.jose.shaded.json.parser.JSONParser;
 import lombok.RequiredArgsConstructor;
@@ -20,7 +21,7 @@ import java.net.URL;
 @RequiredArgsConstructor
 public class ReviewController {
 
-    @GetMapping ("/review/{movieId}")
+    @GetMapping ("/{movieId}")
     public Object review(MovieDTO dto, Model model) {
 
         String key = "";
@@ -28,31 +29,71 @@ public class ReviewController {
         Long movieId = dto.getMovieId();
 
         // 파싱한 데이터를 저장할 변수
-        String result = "";
+        String movieJson = "";
+        String watchJson = "";
+        String castJson = "";
 
         JSONObject movieData = null;
+        JSONObject watchData1 = null;
+        JSONObject watchData2 = null;
+        JSONObject watchData3 = null;
+        JSONArray watchArray = null;
+
+        JSONObject castObject = null;
+
         try {
-            URL url = new URL("https://api.themoviedb.org/3/" +
+            URL movieUrl = new URL("https://api.themoviedb.org/3/" +
                     "movie/" + movieId +
                     "?api_key=" + key +
-                    "&language=ko&append_to_response=credits%2Cvideos%2Cwatch%2Fproviders");
-            //log.info(url+"어디");
+                    "&language=ko");
 
-            BufferedReader bf;
+            URL watchUrl = new URL("https://api.themoviedb.org/3/" +
+                    "movie/" + movieId +
+                    "/watch/providers?" +
+                    "api_key=" + key);
 
-            bf = new BufferedReader(new InputStreamReader(url.openStream()));
+            URL castUrl = new URL("https://api.themoviedb.org/3/" +
+                    "movie/" + movieId +
+                    "/credits?" +
+                    "api_key=" + key +
+                    "&language=ko");
 
-            result = bf.readLine();
+            BufferedReader movieBf;
+            BufferedReader watchBf;
+            BufferedReader castBf;
+
+            movieBf = new BufferedReader(new InputStreamReader(movieUrl.openStream()));
+            watchBf = new BufferedReader(new InputStreamReader(watchUrl.openStream()));
+            castBf  = new BufferedReader(new InputStreamReader(castUrl.openStream()));
+
+            movieJson = movieBf.readLine();
+            watchJson = watchBf.readLine();
+            castJson = castBf.readLine();
 
             JSONParser jsonParser = new JSONParser();
-            movieData = (JSONObject) jsonParser.parse(result);
+            movieData = (JSONObject) jsonParser.parse(movieJson);
 
-            log.info(movieData + "어디");
+            watchData1 = (JSONObject) jsonParser.parse(watchJson);
+            watchData2 = (JSONObject) watchData1.get("results");
+            watchData3 = (JSONObject) watchData2.get("KR");
+            watchArray = (JSONArray) watchData3.get("flatrate");
+
+            JSONObject cast = (JSONObject) jsonParser.parse(castJson);
+            castObject = (JSONObject) cast.get("cast");
+            log.info(castJson+"어디");
+            log.info(cast +"어디");
+            log.info(castObject +"어디2");
+
+            //76 ~ 86 줄 아무 의미없음 안됨 개빡침
+
+
 
         } catch (Exception e) {
             e.printStackTrace();
         }
         model.addAttribute("movieData", movieData);
+        model.addAttribute("watchLink", watchData3);
+        model.addAttribute("watchLogo", watchArray);
         return "/review";
     }
 
