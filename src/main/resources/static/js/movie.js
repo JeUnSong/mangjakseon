6497297f33ef84f7e048254f1021a7cba1ca868c
JeUnsong <zzangjh5@gmail.com>
2022-06-28 19:58:47 +0900

    영화sort 주소 변경

--- src/main/resources/static/js/movie.js
+++ src/main/resources/static/js/movie.js
@@ -5,21 +5,27 @@ const key = ""
 function movieList(){
     $.ajax({
         type: "GET",
-        url: "https://api.themoviedb.org/3/discover/movie?" +
-            `api_key=${key}` +
-            "&language=ko&include_adult=false&include_video=false&" +
-            `page=${page}` +
-            "&vote_average.gte=0.1&vote_average.lte=6&with_watch_monetization_types=flatrate",
+        url: "https://api.themoviedb.org/3/discover/movie" +
+            `?api_key=${key}` +
+            "&language=ko&region=krr%2Cus" +
+            "&sort_by=popularity.desc" +
+            "&include_adult=false" +
+            "&include_video=false" +
+            `&page=${page}` +
+            "&vote_average.gte=1" +
+            "&vote_average.lte=6" +
+            "&with_watch_monetization_types=flatrate",
         data: {},
         dataType:"json",
         success: function(data){
+
             // console.log(data)
             // console.log(data.results)
 
             let list = [];
             data.results.forEach((item)=>{
                 list.push([
-                    str = `<form action=/${item.id} method="GET">`,
+                    str = `<form action=/movie/${item.id} method="GET">`,
                     str += '<div>',
                     str += '<div class="moviePoster">' + `<input type="image" name="poster" src=https://image.tmdb.org/t/p/w500${item.poster_path}>` + '</div>',
                     str += '<div class="movieScore">' + '<span>' + `TMDB ${item.vote_average} 망작선` + '</span>' + '</div>',
@@ -37,7 +43,6 @@ function movieList(){
 $(document).ready(function (){
     movieList();
 })
-
 
 // 글릭시 다음 페이지 갱신
 $(document).ready(function() {
