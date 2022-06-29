e9681aec932d13a75e20950af7a18f8441943c11
JeUnsong <zzangjh5@gmail.com>
2022-06-29 20:03:51 +0900

    배우 페이지 완성

--- src/main/java/com/mangjakseon/controller/ActorInfoController.java
+++ src/main/java/com/mangjakseon/controller/ActorInfoController.java
@@ -30,38 +30,51 @@ public class ActorInfoController {
 
         String actorJson = "";
         String actorMovieJson = "";
+        String snsJson = "";
 
         JSONObject actorData = null;
         JSONObject actormovieData = null;
         JSONArray actorMovieArray = null;
+        JSONObject snsData = null;
 
         try {
             URL actorUrl = new URL("https://api.themoviedb.org/3/" +
                     "person/" + actorId +
                     "?api_key=" + key +
-                    "&language=ko");
+                    "&language=us");
+
             URL actorMovieUrl = new URL("https://api.themoviedb.org/3/" +
                     "person/" + actorId +
                     "/movie_credits" +
                     "?api_key=" + key +
                     "&language=ko");
 
+            URL snsUrl = new URL("https://api.themoviedb.org/3/" +
+                    "person/" + actorId +
+                    "/external_ids" +
+                    "?api_key=" + key +
+                    "&language=ko");
+
             BufferedReader actorBf;
             BufferedReader actorMovieBf;
+            BufferedReader snsBf;
 
 
             actorBf = new BufferedReader(new InputStreamReader(actorUrl.openStream()));
             actorMovieBf = new BufferedReader(new InputStreamReader(actorMovieUrl.openStream()));
+            snsBf = new BufferedReader(new InputStreamReader(snsUrl.openStream()));
 
 
             actorJson = actorBf.readLine();
             actorMovieJson = actorMovieBf.readLine();
+            snsJson = snsBf.readLine();
 
             JSONParser jsonParser = new JSONParser();
 
             actorData = (JSONObject) jsonParser.parse(actorJson);
             actormovieData = (JSONObject) jsonParser.parse(actorMovieJson);
             actorMovieArray = (JSONArray) actormovieData.get("cast");
+            snsData = (JSONObject) jsonParser.parse(snsJson);
 
             log.info(actorMovieArray + "어디");
 
@@ -70,6 +83,7 @@ public class ActorInfoController {
         }
         model.addAttribute("actorData", actorData);
         model.addAttribute("actorMovieData", actorMovieArray);
+        model.addAttribute("snsData", snsData);
 
         return "/actor-info";
     }
