32906b16faa6d083439bbdff988acb35a7b1b237
JeUnsong <zzangjh5@gmail.com>
2022-07-04 17:46:43 +0900

    움직이는 포스터

--- src/main/resources/static/js/review.js
+++ src/main/resources/static/js/review.js
@@ -188,6 +188,30 @@
         });
     })}
 
+    // 움직이는 포스터
+    $(document).ready(function() {
+        var isVisible = false;
+
+        var currentPosition = parseInt($(".movingPoster").css("top"));
+        $(window).scroll(function () {
+            var position = $(window).scrollTop(); // 현재 스크롤바의 위치값을 반환
+            $(".movingPoster").stop().animate({"top": position + currentPosition + "px"}, 1000);
+            if (checkVisible($('.footerCopyright')) && !isVisible) {
+                $(".movingPoster").stop()
+            }
+        });
+
+        function checkVisible(elm, eval) {
+            eval = eval || "object visible";
+            var viewportHeight = $(window).height(), // Viewport Height
+                scrolltop = $(window).scrollTop(), // Scroll Top
+                y = $(elm).offset().top,
+                elementHeight = $(elm).height();
+
+            if (eval == "object visible") return ((y < (viewportHeight + scrolltop)) && (y > (scrolltop - elementHeight)));
+            if (eval == "above") return ((y < (viewportHeight + scrolltop)));
+        }
+    });
 
 
 
