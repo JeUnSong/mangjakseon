c15d30021e7a99f6ce3a93416cab7ef12daedccc
JeUnsong <zzangjh5@gmail.com>
2022-07-06 11:08:25 +0900

    스크롤 아래있을시 맨위로 가는 버튼 추가

--- src/main/resources/static/js/movie.js
+++ src/main/resources/static/js/movie.js
@@ -1,9 +1,10 @@
 var page = 1;
 const key = "";
 var totalStr = "";
-
+var sort = ""
 //ajax data 불러오기
 function movieList() {
+
     $.ajax({
         type: "GET",
         url: "https://api.themoviedb.org/3/discover/movie" +
@@ -87,7 +88,19 @@ $(document).ready(function (event) {
     });
 });
 
-
+$( document ).ready( function() {
+    $( window ).scroll( function() {
+        if ( $( this ).scrollTop() > 200 ) {
+            $( '.top' ).fadeIn();
+        } else {
+            $( '.top' ).fadeOut();
+        }
+    });
+    $( '.top' ).click( function() {
+        $( 'html, body' ).animate( { scrollTop : 0 }, 400 );
+        return false;
+    });
+});
 
 
 
