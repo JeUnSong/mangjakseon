73282451298d67dc8861e2dea870ce6a94e80047
JeUnSong <94476600+JeUnSong@users.noreply.github.com>
2022-07-10 11:49:20 +0900

    비밀번호 유효성 검사 고침

--- src/main/resources/static/js/sign-up.js
+++ src/main/resources/static/js/sign-up.js
@@ -1,11 +1,11 @@
 function passConfirm(){
-    var password = document.getElementById('password');
+    var singPassword = document.getElementById('singPassword');
     var passwordConfirm = document.getElementById('passwordConfirm');
     var confirmMsg = document.getElementById('confirmMsg');
     var correctColor = "#00ff00";
     var incorrectColor = "#ff0000";
 
-    if (password.value == passwordConfirm.value) {
+    if (singPassword.value == passwordConfirm.value) {
         confirmMsg.style.color = correctColor;
         confirmMsg.innerHTML = "비밀번호 일치";
     } else {
